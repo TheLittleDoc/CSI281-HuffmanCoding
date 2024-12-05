1178992ea6fb8c591eb06d9ea1160b0332d84b73
Noah Maywar <160625130+MaywarDeChamplain@users.noreply.github.com>
2024-12-05 14:33:37 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 //
 // Created by elysium.hosack on 11/18/2024.
 //
-
+#define CATCH_CONFIG_MAIN
+#include "catch.h"
 #include "HuffmanCoding.h"
 
 using namespace std;
-
+*/
 int main() {
     HuffmanCoding encoder("this is an example for huffman encodingA");
     encoder.encode();
 
     return 0;
-}
+}*/
